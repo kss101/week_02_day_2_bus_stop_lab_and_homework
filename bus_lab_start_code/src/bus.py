4a45ada7b9c1b770448309d6e9a4a30e6f666a32
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-10 15:58:00 +0000

    Initial commit and created Bus class

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,7 @@
+class Bus:
+    def __init__( self, route_num, destination):
+        self.route_num = route_num
+        self.destination = destination
+
+    def drive( self ):
+        return "Brun burm... on to the next stop!"
